ef39c0c77870edb73fa1901dab0c8463967c789b
George Ward <gwardgaming@gmail.com>
2025-12-03 14:41:57 +0000

    My solution to day 1 part 2 of the Advent of Code 2025.

--- day-1/day1.py
+++ day-1/day1.py
@@ -4,22 +4,34 @@ def findPassword(input_file):
     try:
         with open(input_file, "r") as file:
             for line in file:
-                print(dial_num, count, line)
                 dial_num, count = assess(line, dial_num, count)
+            print(dial_num, count, line)
     except FileNotFoundError:
         print("Error: File not found.")
 
 def assess(textLine, dial_num, count):
-    num = textLine[1:]
+    num = int(textLine[1:])
+
     if textLine[0] == "L":
-        dial_num = (dial_num - int(num)) % 100
+        
+        for _ in range(num):
+            dial_num -= 1
+            if dial_num % 100 == 0:
+                count += 1
+
+        dial_num = dial_num % 100
+                
     elif textLine[0] == "R":
-        dial_num = (dial_num + int(num)) % 100
+        
+        for _ in range(num):
+            dial_num += 1
+            if dial_num % 100 == 0:
+                count += 1
+
+        dial_num = dial_num % 100
+
     else:
         print("Error: Invalid line format.")
-    if dial_num == 0:
-        count += 1
     return dial_num, count
     
-
 findPassword("input.txt")
